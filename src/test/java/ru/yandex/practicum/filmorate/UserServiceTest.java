341588d4bcf1585a6d000dfc2b9e53801b0cb349
Dmitry <dm1try-teter1n@yandex.ru>
2023-03-01 21:18:41 +0500

    restyle

---
+++ src/test/java/ru/yandex/practicum/filmorate/UserServiceTest.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.controllers.UserController;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
+
+import java.time.LocalDate;
+
+public class UserServiceTest {
+
+
+}
