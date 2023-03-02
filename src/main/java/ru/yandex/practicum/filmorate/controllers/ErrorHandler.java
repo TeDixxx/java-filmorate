ed61f3c97285a22a6b090ca2e4d7d2bc80708763
Dmitry <dm1try-teter1n@yandex.ru>
2023-03-02 18:56:29 +0500

    restyle

--- src/main/java/ru/yandex/practicum/filmorate/controllers/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/ErrorHandler.java
@@ -1,59 +1,13 @@
 package ru.yandex.practicum.filmorate.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 
-import java.util.Map;
-import java.util.NoSuchElementException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice("ru.yandex.practicum.filmorate.controller")
 public class ErrorHandler {
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> handleNoFoundFilmException(final NoSuchElementException e) {
-        log.error(e.getMessage());
-        return Map.of(
-                "error", "Фильм не найден",
-                "errorMessage", e.getMessage()
-        );
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> handleFoundUserException(final NoSuchElementException e) {
-        log.error(e.getMessage());
-        return Map.of(
-                "error", "Пользователь не найден",
-                "errorMessage", e.getMessage()
-        );
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidationException(final ValidationException e) {
-        log.error(e.getMessage());
-        return Map.of(
-                "error", "Ошибка",
-                "errorMessage", e.getMessage()
-        );
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> handleException(final Exception e) {
-        log.error(e.getMessage());
-        return Map.of(
-                "error", "Ошибка",
-                "errorMessage", e.getMessage()
-        );
-    }
 
 
 }
