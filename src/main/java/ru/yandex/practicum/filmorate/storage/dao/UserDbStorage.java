86f0b80bb47b67a4dcca6590d1413007fca4e1aa
TeDixxx <114980863+TeDixxx@users.noreply.github.com>
2023-03-30 12:33:25 +0500

    Update UserDbStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/UserDbStorage.java
@@ -38,7 +38,7 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public User updateUser(User user) throws NotFoundException {
-        String sqlQuery = "UPDATE users" + "SET name = ?, email = ?,birthday = ?" + "WHERE user_id = ?";
+        String sqlQuery = "UPDATE users SET name = ?, email = ?,birthday = ? WHERE user_id = ?";
 
         jdbcTemplate.update(sqlQuery, user.getName(), user.getEmail(), user.getBirthday());
         return user;
@@ -46,21 +46,21 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public User getUser(Long id) throws NotFoundException {
-        String sqlQuery = "SELECT user_id,login,email,name,birthday" + "FROM users" + "WHERE user_id = ?";
+        String sqlQuery = "SELECT user_id,login,email,name,birthday FROM users WHERE user_id = ?";
 
         return jdbcTemplate.queryForObject(sqlQuery, this::mapRowToUser, id);
     }
 
     @Override
     public void removeUser(User user) {
-        String sqlQuery = "DELETE FROM users" + "WHERE user_id = ?";
+        String sqlQuery = "DELETE FROM users WHERE user_id = ?";
         jdbcTemplate.update(sqlQuery, user.getId());
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        String sqlQuery = "SELECT*" + "FROM users";
+        String sqlQuery = "SELECT* FROM users";
 
         return jdbcTemplate.query(sqlQuery,this::mapRowToUser);
     }
