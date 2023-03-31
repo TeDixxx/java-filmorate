3cf9869a23bbf22df0832703d7484cc0d6ef51fd
Dmitry <dm1try-teter1n@yandex.ru>
2023-03-31 23:20:06 +0500

    Fix codestyle

--- src/main/java/ru/yandex/practicum/filmorate/controllers/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/ErrorHandler.java
@@ -6,9 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.yandex.practicum.filmorate.exception.IncorrectParameterException;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.exception.*;
+import ru.yandex.practicum.filmorate.model.ErrorResponse;
 
 import java.util.Map;
 
@@ -42,6 +41,18 @@ public class ErrorHandler {
     public Map<String, String> handleValidationException(final ValidationException e) {
         log.warn("Ошибка! NullPointerException {}", e.getMessage());
         return Map.of("NullPointerException", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleMpaNotFoundException(final MpaNotFoundException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleGenreNotFoundException(final GenreNotFoundException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/GenreNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.exception;
+
+public class GenreNotFoundException extends IllegalArgumentException{
+    public GenreNotFoundException(String message) {
+        super(message);
+    }
+}
