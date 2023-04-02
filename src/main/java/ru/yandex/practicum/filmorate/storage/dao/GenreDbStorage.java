8a1671db691162eab157047ec3168b423c805ec7
Dmitry <dm1try-teter1n@yandex.ru>
2023-04-02 23:10:38 +0500

    add optional

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/GenreDbStorage.java
@@ -4,7 +4,6 @@ package ru.yandex.practicum.filmorate.storage.dao;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.interfaces.GenreStorage;
 
