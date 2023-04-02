5a76752a7792b56023d55f43ec8ba11ce4a13cb9
Dmitry <dm1try-teter1n@yandex.ru>
2023-04-02 22:32:14 +0500

    add optional

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.service;
 
 
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
+
 
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.interfaces.GenreStorage;
